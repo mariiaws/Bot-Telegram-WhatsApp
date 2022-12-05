e82ee031337cefd00628018222fce8b79bc3c19f
mariiaws <wooglay@gmail.com>
2022-12-05 14:59:08 +0300

    Created a drop down list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 'use strict'
+
+let dropDown = document.querySelector('.nav__language');
+let dropDownContent = document.querySelector('.drop-down-content');
+
+let languageRussia = document.querySelector('.language-russia');
+let flagRussia = document.querySelector('.svg-flag-russia');
+let flagUk = document.querySelector('.svg-flag-uk');
+
+
+dropDown.addEventListener('click', function () {
+    console.log('click');
+
+    if (dropDownContent.style.cssText === 'display: none;') {
+        dropDownContent.style.cssText = 'display: flex;';
+
+    } else dropDownContent.style.cssText = 'display: none;';
+
+})
+
+languageRussia.onclick = function () {
+    if (flagRussia.style.cssText === 'display: none;') {
+        flagRussia.style.cssText = 'display: flex;';
+        flagUk.style.cssText = 'display: none;';
+
+    } else {
+        flagRussia.style.cssText = 'display: none;';
+        flagUk.style.cssText = 'display: flex;';
+    }
+
+}
